2c12b55ac2f703acc78d208d1aa9421a90dab3c2
DarkVasDehors <sibade.arnaud@gmail.com>
2017-12-08 01:53:01 +0100

    GetDate+ recupCookie

--- cookie_arnaud/test.js
+++ cookie_arnaud/test.js
@@ -1,22 +1,62 @@
-function test() {
-  document.getElementById("test").addEventListener("click", checkDate);
 
+window.onload = function() {
+  let body = document.querySelector("body");
+  document.querySelector(".test").addEventListener("click", checkDate);
+  document.querySelector(".return").addEventListener("click", getCookie);
 }
+//document.getElementById("test").addEventListener("click", checkDate);
+addCookie("1");
 
 function addCookie(j) {
   document.cookie = "dateOpen"+j+"= true";
+
 }
 
 function checkDate(event){
-  let month = getMonth();
-  let jour = getDate();
-
+  let date = new Date();
+  let month = date.getMonth();
+  let jour = date.getDate();
   if(month==11){
     if(event.id<=jour){
-      
-      addCookie(j);
-
+      if(notOpen){ //TODO
+        addCookie(j);
+        //TODO openCase
+      }
     }
   }
 
+}
+
+function getCookies() {
+    //let name = dateJ + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie);
+    let ca = decodedCookie.split(';');
+    console.log(ca);
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        //if (c.indexOf(name) == 0) {
+        //    return c.substring(name.length, c.length);
+        //}
+    }
+}
+
+function getCookie(dateJ) {
+    let name = dateJ + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie);
+    let ca = decodedCookie.split(';');
+    console.log(ca);
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
 }
